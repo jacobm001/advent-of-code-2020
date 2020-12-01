a4f720b8b6f9c97c60cd32c18524bb0ee0cdcb07
Jacob Mastel <jacobm001@gmail.com>
2020-12-01 11:00:51 -0800

    Added calls for puzzle 1 and 2. Day 1 complete.

--- day01.py
+++ day01.py
@@ -1,4 +1,5 @@
 from itertools import combinations
+from common import file_to_int_array
 
 
 def product(arr: list):
@@ -11,10 +12,20 @@ def product(arr: list):
 
 
 def find_values(arr: list, expected_value: int, group_size: int = 2):
-    for group in combinations(arr, 2):
+    for group in combinations(arr, group_size):
         if sum(group) == expected_value:
             return product(group)
 
+
 if __name__ == "__main__":
-    v = find_values([1721,979,366,299,675,1456], 2020)
-    print(v)
+    input_file = 'day01.txt'
+    input_array = file_to_int_array(input_file)
+
+    # Puzzle 1
+    res1 = find_values(input_array, 2020)
+    print(f'Puzzle1 Answer: {res1}')
+
+    # Puzzle 2
+    res2 = find_values(input_array, 2020, 3)
+    print(f'Puzzle2 Answer: {res2}')
+
