2bcaa880079116394e5dad9dc0225aa6910d9496
Jacob Mastel <jacob.mastel@oregonstate.edu>
2020-12-04 22:16:33 -0800

    built ticket parser

--- day05-test.py
+++ day05-test.py
@@ -1,10 +1,30 @@
 import unittest
+from common import TestValue
 import day05
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_assigned_seat(self):
+        tests = [
+            TestValue('FBFBBFFRLR', (44, 5))
+            , TestValue('BFFFBBFRRR', (70, 7))
+            , TestValue('FFFBBBFRRR', (14, 7))
+            , TestValue('BBFFBBFRLL', (102, 4))
+        ]
+        for test in tests:
+            t = day05.TicketParser(test.input_value)
+            self.assertEqual(t.get_seat(), test.expected_output)
+
+    def test_seat_id(self):
+        tests = [
+            TestValue('FBFBBFFRLR', 357)
+            , TestValue('BFFFBBFRRR', 567)
+            , TestValue('FFFBBBFRRR', 119)
+            , TestValue('BBFFBBFRLL', 820)
+        ]
+        for test in tests:
+            t = day05.TicketParser(test.input_value)
+            self.assertEqual(t.seat_id, test.expected_output)
 
 
 if __name__ == '__main__':
--- day05.py
+++ day05.py
@@ -1,4 +1,68 @@
+import math
+import re
 import common
+
+NUM_ROWS: int = 128
+NUM_COLS: int = 8
+
+TICKET_PATTERN: re.Pattern = re.compile('^([FB]+)([LR]+)$')
+
+
+class TicketParser():
+    seat_code: str
+    seat_id: int
+    assigned_row: int
+    assigned_col: int
+
+    def __init__(self, seat_str: str):
+        self.seat_code = seat_str
+
+        match = re.match(TICKET_PATTERN, seat_str)
+        self.find_row(match.group(1))
+        self.find_col(match.group(2))
+        self.calc_seat_id()
+
+    def find_row(self, row_code):
+        min_row: int = 0
+        max_row: int = NUM_ROWS - 1
+
+        for i, letter in enumerate(row_code):
+            if i == len(row_code) - 1:
+                if letter == 'F':
+                    self.assigned_row = min_row
+                elif letter == 'B':
+                    self.assigned_row = max_row
+
+                return
+
+            if letter == 'F':
+                max_row = math.floor((max_row + min_row) / 2)
+            elif letter == 'B':
+                min_row = math.ceil((max_row + min_row) / 2)
+
+    def find_col(self, col_code):
+        min_col: int = 0
+        max_col: int = NUM_COLS - 1
+
+        for i, letter in enumerate(col_code):
+            if i == len(col_code) - 1:
+                if letter == 'L':
+                    self.assigned_col = min_col
+                elif letter == 'R':
+                    self.assigned_col = max_col
+
+                return
+
+            if letter == 'L':
+                max_col = math.floor((max_col + min_col) / 2)
+            elif letter == 'R':
+                min_col = math.ceil((max_col + min_col) / 2)
+
+    def get_seat(self) -> tuple:
+        return self.assigned_row, self.assigned_col
+
+    def calc_seat_id(self):
+        self.seat_id = self.assigned_row * 8 + self.assigned_col
 
 
 if __name__ == "__main__":
