d0e9d31045ab2527cb882a0c938f7b2c7825e2d7
Jacob Mastel <jacob.mastel@oregonstate.edu>
2020-12-04 20:30:01 -0800

    day05 prepped

---
+++ day05-test.py
@@ -0,0 +1,11 @@
+import unittest
+import day05
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
