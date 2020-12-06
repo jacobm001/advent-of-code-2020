96b3dcb9d87a26cac63efcbe101e1e46032a8aa7
Jacob Mastel <jacob.mastel@oregonstate.edu>
2020-12-05 22:59:24 -0800

    Finished day06

--- common/inputs.py
+++ common/inputs.py
@@ -14,6 +14,16 @@ def read_list(f: str, type_func = int):
     return arr
 
 
+def read_blocks(f: str, type_func = str) -> List[str]:
+    path = f'inputs/{f}'
+    with open(path, 'r') as f:
+        raw_str = f.read()
+
+    arr = raw_str.split('\n\n')
+    arr = list(map(type_func, arr))
+    return arr
+
+
 def read_matrix(f: str, type_func = int) -> Matrix:
     path = f'inputs/{f}'
     with open(path, 'r') as f:
