d8147c926bf3566561272dd9334119f66a83197f
Jacob Mastel <jacobm001@gmail.com>
2020-12-08 13:40:23 -0800

    Finished day08 part 2

--- day08.py
+++ day08.py
@@ -21,9 +21,9 @@ class BootCode:
     cur_position: int
 
     def __init__(self, input_instructions: List[str]):
-        self.accumulator    = 0
-        self.cur_position   = 0
-        self.seen_positions = []
+        self.accumulator       = 0
+        self.cur_position      = 0
+        self.seen_positions    = []
 
         self.cmd_translate = {
             'acc': self.accumulate
@@ -31,7 +31,8 @@ class BootCode:
             , 'nop': self.noop
         }
 
-        self.instructions = input_instructions
+        self.instructions          = input_instructions
+        self.original_instructions = input_instructions
 
     def accumulate(self, value) -> None:
         self.accumulator += value
@@ -43,30 +44,33 @@ class BootCode:
     def noop(self, value) -> None:
         self.cur_position += 1
 
-    def process(self) -> None:
+    def process(self, try_swap: bool = False) -> None:
         instruction = self.instructions[self.cur_position]
-        cmd, arg = parse_instruction(instruction)
+        cmd, arg    = parse_instruction(instruction)
 
         self.seen_positions.append(self.cur_position)
         self.cmd_translate[cmd](arg)
 
     def check_for_exit(self) -> bool:
-        if self.cur_position == len(self.instructions):
+        if self.cur_position >= len(self.instructions) - 1:
             return True
         else:
             return False
 
-    def process_first_loop(self) -> int:
-        while self.cur_position not in self.seen_positions:
+    def reset(self):
+        self.accumulator    = 0
+        self.cur_position   = 0
+        self.seen_positions = []
+
+    def process_first_loop(self) -> bool:
+        while True:
             self.process()
 
             if self.cur_position in self.seen_positions:
-                break
+                return False
 
             if self.check_for_exit():
-                break
-
-        return self.accumulator
+                return True
 
 
 if __name__ == '__main__':
@@ -74,7 +78,24 @@ if __name__ == '__main__':
     input_list = common.read_list(f, str)
     bc         = BootCode(input_list)
 
-    answer1 = bc.process_first_loop()
-
+    bc.process_first_loop()
+    answer1 = bc.accumulator
     print(f'Answer 1: {answer1}')
 
+    changeable_spaces = []
+    for i, line in enumerate(input_list):
+        if line[:3] in ['nop', 'jmp']:
+            changeable_spaces.append(i)
+
+    for space in changeable_spaces:
+        instructions = input_list.copy()
+        if instructions[space][:3] == 'nop':
+            cmd = 'jmp'
+        else:
+            cmd = 'nop'
+
+        instructions[space] = cmd + instructions[space][3:]
+        bc2 = BootCode(instructions)
+        if bc2.process_first_loop():
+            answer2 = bc2.accumulator
+            print(f'Answer 2: {answer2}')
