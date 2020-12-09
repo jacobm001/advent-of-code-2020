c7483c7d91d97335ac2543258dba35f4284c2602
Jacob Mastel <jacobm001@gmail.com>
2020-12-09 11:27:54 -0800

    Solved part 2

--- day09-test.py
+++ day09-test.py
@@ -12,7 +12,14 @@ class MyTestCase(unittest.TestCase):
         xmas = day09.XMAS(self.test_input, 5)
         ret  = xmas.find_first_invalid_entry()
 
-        self.assertEqual(ret, test.expected_output)
+        self.assertEqual(test.expected_output, ret)
+
+    def test_find_encryption_weakness(self):
+        test = common.TestValue(127, 62)
+        xmas = day09.XMAS(self.test_input, 5)
+
+        weakness = xmas.find_encryption_weakness(test.input_value)
+        self.assertEqual(test.expected_output, weakness)
 
 
 if __name__ == '__main__':
--- day09.py
+++ day09.py
@@ -23,6 +23,20 @@ class XMAS:
             if not f:
                 return self.code[i]
 
+    def find_encryption_weakness(self, invalid_entry) -> int:
+        """ Uses a nested for loop to try and find the sublist that totals the submitted invalid entry """
+
+        stop: int  = self.code.index(invalid_entry)
+
+        for i in range(0, stop):
+            total: int = self.code[i]
+
+            for j in range(i+1, stop):
+                total += self.code[j]
+
+                if total == invalid_entry:
+                    values = self.code[i:j]
+                    return min(values) + max(values)
 
 
 if __name__ == '__main__':
@@ -31,3 +45,6 @@ if __name__ == '__main__':
 
     answer1 = xmas.find_first_invalid_entry()
     print(f'Answer 1: {answer1}')
+
+    answer2 = xmas.find_encryption_weakness(answer1)
+    print(f'Answer 2: {answer2}')
