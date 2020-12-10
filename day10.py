08040e4f0121f9bd2f561cd4b102e3464fd1d624
Jacob Mastel <jacob.mastel@oregonstate.edu>
2020-12-09 22:46:03 -0800

    day10 part 2 started, but not finished

--- day10.py
+++ day10.py
@@ -27,11 +27,27 @@ def get_differences(il: List[int]) -> collections.Counter:
     return collections.Counter(differences)
 
 
+def count_branches(il: List[int], position: int = 0):
+    count = 1
+    start = position + 1
+    end   = len(il) - start
+    for i in range(start, end+1):
+        if il[i] - il[position] <= 3:
+            count += count_branches(il, i+1)
+
+    return count
+
+
 def part1(il) -> int:
     il = prep_list(il)
     difference_count = get_differences(il)
 
     return difference_count[1] * difference_count[3]
+
+
+def part2(il) -> int:
+    il = prep_list(il)
+    return count_branches(input_list) + 1
 
 
 if __name__ == '__main__':
@@ -40,3 +56,6 @@ if __name__ == '__main__':
 
     answer1: int = part1(input_list)
     print(f'Answer 1: {answer1}')
+
+    answer2: int = part2(input_list)
+    print(f'Answer 2: {answer2}')
