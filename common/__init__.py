b93530cf4a517aeb6b7e1f60b30f25a651c07417
Jacob Mastel <jacob.mastel@oregonstate.edu>
2020-12-10 22:53:45 -0800

    Added matrix_equal() function

--- common/__init__.py
+++ common/__init__.py
@@ -15,3 +15,12 @@ def product(arr: IntList):
 def print_matrix(matrix: Matrix):
     for row in matrix:
         print(row)
+
+
+def matrix_equal(matrix_a: Matrix, matrix_b: Matrix) -> bool:
+    for x in range(0, len(matrix_a)):
+        for y in range(0, len(matrix_a[0])):
+            if matrix_a[x][y] != matrix_b[x][y]:
+                return False
+
+    return True
