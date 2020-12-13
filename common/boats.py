cf1f4809ee6991be908ad9255af67c5de055cd39
Jacob Mastel <jacobm001@gmail.com>
2020-12-12 23:10:53 -0800

    Finished part 2

--- common/boats.py
+++ common/boats.py
@@ -1,4 +1,5 @@
 import enum
+from math import sin, cos, radians
 
 
 class CompassPoints(enum.Enum):
@@ -59,6 +60,68 @@ class Boat:
             self.heading -= 360
         elif self.heading < 0:
             self.heading += 360
+
+        return
+
+    def manhattan_distance(self) -> int:
+        return abs(self.pos_north) + abs(self.pos_east)
+
+
+class WaywardBoat:
+    waypoint_pos_north: int
+    waypoint_pos_east: int
+    pos_north: int
+    pos_east: int
+
+    def __init__(self):
+        self.waypoint_pos_north = 1
+        self.waypoint_pos_east  = 10
+        self.pos_north = 0
+        self.pos_east  = 0
+
+    def move(self, instruction: str):
+        direction: str = instruction[:1]
+        distance: int = int(instruction[1:])
+
+        if direction == 'N':
+            self.waypoint_pos_north += distance
+        elif direction == 'S':
+            self.waypoint_pos_north -= distance
+        elif direction == 'E':
+            self.waypoint_pos_east += distance
+        elif direction == 'W':
+            self.waypoint_pos_east -= distance
+        elif direction == 'F':
+            self.move_forward(distance)
+        elif direction in ('L', 'R'):
+            self.turn(direction, distance)
+        else:
+            raise ValueError(f'{direction} is not a valid direction')
+
+    def move_forward(self, distance: int):
+        for i in range(0, distance):
+            self.pos_north += self.waypoint_pos_north
+            self.pos_east  += self.waypoint_pos_east
+
+    def turn(self, direction: str, degrees: int):
+        """
+            Uses matrix rotation to rotate the coordinates.
+            See: https://en.wikipedia.org/wiki/Rotation_matrix
+        """
+        # this is counter-clockwise, which our formula is setup for
+        if direction == 'L':
+            pass
+        # this is a clockwise rotation, convert to counter-clockwise
+        elif direction == 'R':
+            degrees = (0 - degrees) % 360
+        else:
+            raise ValueError("Invalid direction, %s", direction)
+
+        angle = radians(degrees)
+        x = self.waypoint_pos_east
+        y = self.waypoint_pos_north
+        self.waypoint_pos_east  = round(x * cos(angle) - y * sin(angle))
+        self.waypoint_pos_north = round(x * sin(angle) + y * cos(angle))
 
         return
 
--- day12.py
+++ day12.py
@@ -10,3 +10,12 @@ if __name__ == '__main__':
 
     answer1: int = ship.manhattan_distance()
     print(f'Answer 1: {answer1}')
+
+    ship2 = common.WaywardBoat()
+
+    for move in moves:
+        ship2.move(move)
+
+    answer2: int = ship2.manhattan_distance()
+    print(f'Answer2: {answer2}')
+
