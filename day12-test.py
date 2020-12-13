482bc9aced623fea7617163b21597e941310dfb6
Jacob Mastel <jacobm001@gmail.com>
2020-12-12 21:41:45 -0800

    Solved day12 part 1

---
+++ day12-test.py
@@ -0,0 +1,50 @@
+import unittest
+import day12
+
+class MyTestCase(unittest.TestCase):
+    def test_boat_init(self):
+        boat = day12.Boat()
+        self.assertEqual(90, boat.heading)
+        self.assertEqual(0, boat.pos_north)
+        self.assertEqual(0, boat.pos_east)
+
+    def test_move(self):
+        boat = day12.Boat()
+        boat.move('N10')
+        boat.move('E10')
+        self.assertEqual(90, boat.heading)
+        self.assertEqual(10, boat.pos_north)
+        self.assertEqual(10, boat.pos_east)
+        boat.move('S8')
+        boat.move('W5')
+        self.assertEqual(90, boat.heading)
+        self.assertEqual(2, boat.pos_north)
+        self.assertEqual(5, boat.pos_east)
+
+    def test_move_forward(self):
+        boat = day12.Boat()
+        boat.move('F10')
+        self.assertEqual(90, boat.heading)
+        self.assertEqual(0, boat.pos_north)
+        self.assertEqual(10, boat.pos_east)
+
+    def test_turn(self):
+        boat = day12.Boat()
+        boat.move('L90')
+        self.assertEqual(0, boat.heading)
+        self.assertEqual(0, boat.pos_north)
+        self.assertEqual(0, boat.pos_east)
+
+        boat.move('R180')
+        self.assertEqual(180, boat.heading)
+        self.assertEqual(0, boat.pos_north)
+        self.assertEqual(0, boat.pos_east)
+
+        boat.move('R180')
+        self.assertEqual(0, boat.heading)
+        self.assertEqual(0, boat.pos_north)
+        self.assertEqual(0, boat.pos_east)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ day12.py
@@ -0,0 +1,79 @@
+import common
+import enum
+
+
+class CompassPoints(enum.Enum):
+    NORTH: int = 0
+    SOUTH: int = 180
+    EAST:  int = 90
+    WEST:  int = 270
+
+
+class Boat:
+    heading: int
+    pos_north: int
+    pos_east:  int
+
+    def __init__(self, start_heading: int = 90):
+        self.heading   = start_heading
+        self.pos_north = 0
+        self.pos_east  = 0
+
+    def move(self, instruction: str):
+        direction: str = instruction[:1]
+        distance: int  = int(instruction[1:])
+
+        if direction == 'N':
+            self.pos_north += distance
+        elif direction == 'S':
+            self.pos_north -= distance
+        elif direction == 'E':
+            self.pos_east += distance
+        elif direction == 'W':
+            self.pos_east -= distance
+        elif direction == 'F':
+            self.move_forward(distance)
+        elif direction in ('L', 'R'):
+            self.turn(direction, distance)
+        else:
+            raise ValueError(f'{direction} is not a valid direction')
+
+    def move_forward(self, distance: int):
+        if self.heading == CompassPoints.NORTH.value:
+            self.pos_north += distance
+        elif self.heading == CompassPoints.SOUTH.value:
+            self.pos_north -= distance
+        elif self.heading == CompassPoints.EAST.value:
+            self.pos_east += distance
+        elif self.heading == CompassPoints.WEST.value:
+            self.pos_east -= distance
+
+    def turn(self, direction: str, degrees: int):
+        if direction == 'L':
+            self.heading -= degrees
+        elif direction == 'R':
+            self.heading += degrees
+        else:
+            raise ValueError("Invalid direction, %s", direction)
+
+        if self.heading >= 360:
+            self.heading -= 360
+        elif self.heading < 0:
+            self.heading += 360
+
+        return
+
+    def manhattan_distance(self) -> int:
+        return abs(self.pos_north) + abs(self.pos_east)
+
+
+if __name__ == '__main__':
+    f     = 'day12.txt'
+    moves = common.read_list(f, str)
+    ship  = Boat()
+
+    for move in moves:
+        ship.move(move)
+
+    answer1: int = ship.manhattan_distance()
+    print(f'Answer 1: {answer1}')
