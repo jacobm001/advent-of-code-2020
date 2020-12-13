ad143632a6779d5fb4b1cd4c855f8628acbea996
Jacob Mastel <jacobm001@gmail.com>
2020-12-13 14:45:02 -0800

    finished day13 part 1

--- day13.py
+++ day13.py
@@ -6,27 +6,24 @@ if __name__ == '__main__':
     buses_raw: List[Any]
     buses: List[int]
 
-    with open('inputs/day13-test.txt', 'r') as f:
-        earliest  = int(f.readline())
+    with open('inputs/day13.txt', 'r') as f:
+        earliest  = int(f.readline().strip())
         buses_raw = f.readline().strip().split(',')
 
     buses_raw = list(filter(lambda x: x.isnumeric(), buses_raw))
     buses     = list(map(int, buses_raw))
 
-    rounds: int = 1
+    start: int = earliest
+
     answer1: int = None
-    while True:
-        round_times: List = list(map(lambda x: (x * rounds)-1, buses))
-        min_time: int     = min(round_times)
+    while answer1 is None:
+        for i, bus in enumerate(buses):
+            if start % bus == 0:
+                wait: int    = start - earliest
+                bus_id: int  = buses[i]
+                answer1: int = wait * bus_id
+                break
 
-        if earliest <= min_time:
-            pos: int    = round_times.index(min_time)
-            bus_id: int = buses[pos]
-            wait: int   = min_time - earliest
-
-            answer1: int = wait * bus_id
-            break
-
-        rounds += 1
+        start += 1
 
     print(f'Answer 1: {answer1}')
