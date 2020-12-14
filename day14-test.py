c00cfce1ab0bcae4ba2f85dbcc7480a39bc34e87
Jacob Mastel <jacobm001@gmail.com>
2020-12-14 15:04:11 -0800

    finished part 2. Not as elegant as I'd like, but it's alright.

--- day14-test.py
+++ day14-test.py
@@ -1,4 +1,5 @@
 import unittest
+import re
 import day14
 
 
@@ -11,6 +12,29 @@ class MyTestCase(unittest.TestCase):
         cs.set_memory(8, 0)
 
         self.assertEqual(165, cs.get_memory_sum())
+
+    def test_regex_mask(self):
+        instruction: str = 'mask = X100110110X011000101000101XX11001X11'
+        match: re.Match  = re.match(day14.MASK_PATTERN, instruction)
+
+        self.assertEqual('X100110110X011000101000101XX11001X11', match.group(1))
+
+    def test_regex_mem(self):
+        instruction: str = 'mem[5201] = 1838761'
+        match: re.Match  = re.match(day14.MEM_PATTERN, instruction)
+
+        self.assertEqual('5201', match.group(1))
+        self.assertEqual('1838761', match.group(2))
+
+    def test_memory_masking(self):
+        instruction0: str = 'mask = 000000000000000000000000000000X1001X'
+        instruction1: str = 'mem[42] = 100'
+
+        cs = day14.ComputerSystem()
+        cs.set_mask(list('000000000000000000000000000000X1001X'))
+        cs.memory_decoder(42, 100)
+
+        self.assertEqual(100, cs.mem[26])
 
 
 if __name__ == '__main__':
--- day14.py
+++ day14.py
@@ -1,7 +1,14 @@
 import common
+import re
 from typing import List, Dict
+import itertools
+
 Mask   = List[str]
 Memory = Dict[int, str]
+
+
+MASK_PATTERN = re.compile(r'^mask = ([X01]{36})$')
+MEM_PATTERN  = re.compile(r'^mem\[([0-9]+)\] = ([0-9]+)$')
 
 
 class ComputerSystem:
@@ -25,11 +32,46 @@ class ComputerSystem:
 
         return return_value
 
-    def set_memory(self, key: int, value: int):
-        binary_rep    = '{0:036b}'.format(value)
-        binary_rep    = list(binary_rep)
-        masked_value  = self.apply_mask(binary_rep)
-        self.mem[key] = masked_value
+    def set_memory(self, address: int, value: int):
+        binary_rep        = '{0:036b}'.format(value)
+        binary_rep        = list(binary_rep)
+        masked_value      = self.apply_mask(binary_rep)
+        self.mem[address] = masked_value
+
+    def apply_memory_mask(self, binary_rep: List[str]) -> str:
+        return_value = ['0'] * 36
+        for i in range(len(binary_rep)-1, -1, -1):
+            if self.mask[i] == '0':
+                return_value[i] = binary_rep[i]
+            elif self.mask[i] == '1':
+                return_value[i] = '1'
+            else:
+                return_value[i] = 'X'
+
+        return return_value
+
+    def _get_memory_combinations(self, applied_mask: List[str]) -> List[int]:
+        indices = [i for i, x in enumerate(applied_mask) if x == 'X']
+        combos  = list(itertools.product('01', repeat=len(indices)))
+
+        new_list: List[int] = []
+        for combo in combos:
+            new_mask = applied_mask.copy()
+            for i, index in enumerate(indices):
+                new_mask[index] = combo[i]
+
+            s = ''.join(new_mask)
+            new_list.append(int(s, 2))
+
+        return new_list
+
+    def memory_decoder(self, key: int, value: int):
+        mem_binary_rep = '{0:036b}'.format(key)
+        applied_mask   = self.apply_memory_mask(list(mem_binary_rep))
+
+        for address in self._get_memory_combinations(applied_mask):
+            self.mem[address] = value
+
 
     def get_memory_sum(self) -> int:
         # iterate through the memory dictionary values
@@ -39,12 +81,45 @@ class ComputerSystem:
         return sum(list(map(lambda x: int(''.join(x), 2), self.mem.values())))
 
 
-if __name__ == '__main__':
-    cs = ComputerSystem()
-    cs.set_mask(list('XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X'))
-    cs.set_memory(8, 11)
-    cs.set_memory(7, 101)
-    cs.set_memory(8, 0)
+def part1(instructions: List[str]) -> int:
+    cs: ComputerSystem = ComputerSystem()
 
-    answer1 = cs.get_memory_sum()
+    for instruction in instructions:
+        if instruction[0:3] == 'mas':
+            match = re.match(MASK_PATTERN, instruction)
+            if match:
+                cs.set_mask(match.group(1))
+
+        elif instruction[0:3] == 'mem':
+            match = re.match(MEM_PATTERN, instruction)
+            if match:
+                cs.set_memory(int(match.group(1)), int(match.group(2)))
+
+    return cs.get_memory_sum()
+
+
+def part2(instructions: List[str]) -> int:
+    cs: ComputerSystem = ComputerSystem()
+
+    for instruction in instructions:
+        if instruction[0:3] == 'mas':
+            match = re.match(MASK_PATTERN, instruction)
+            if match:
+                cs.set_mask(match.group(1).strip())
+
+        elif instruction[0:3] == 'mem':
+            match = re.match(MEM_PATTERN, instruction)
+            if match:
+                cs.memory_decoder(int(match.group(1)), int(match.group(2)))
+
+    return sum(cs.mem.values())
+
+
+if __name__ == '__main__':
+    instructions: List[str] = common.read_list('day14.txt', str)
+
+    answer1 = part1(instructions)
     print(f'Answer 1: {answer1}')
+
+    answer2 = part2(instructions)
+    print(f'Answer 2: {answer2}')
