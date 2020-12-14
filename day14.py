9c1d495ef3624181685016971367c17cc7c21d39
Jacob Mastel <jacob.mastel@oregonstate.edu>
2020-12-13 21:47:42 -0800

    day14 works for the example

---
+++ day14.py
@@ -0,0 +1,48 @@
+import common
+from typing import List, Dict
+Mask   = List[str]
+Memory = Dict[int, str]
+
+
+class ComputerSystem:
+    mask: Mask
+    mem: Memory
+
+    def __init__(self):
+        self.mask = ['X'] * 36
+        self.mem  = {}
+
+    def set_mask(self, new_mask: Mask):
+        self.mask = new_mask
+
+    def apply_mask(self, binary_rep: List[str]) -> str:
+        return_value = ['0'] * 36
+        for i in range(len(binary_rep)-1, -1, -1):
+            if self.mask[i] != 'X':
+                return_value[i] = self.mask[i]
+            else:
+                return_value[i] = binary_rep[i]
+
+        return return_value
+
+    def set_memory(self, key: int, value: int):
+        binary_rep    = '{0:036b}'.format(value)
+        binary_rep    = list(binary_rep)
+        masked_value  = self.apply_mask(binary_rep)
+        self.mem[key] = masked_value
+
+
+if __name__ == '__main__':
+    cs = ComputerSystem()
+    cs.set_mask(list('XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X'))
+    cs.set_memory(8, 11)
+    cs.set_memory(7, 101)
+    cs.set_memory(8, 0)
+
+    for key, value in cs.mem.items():
+        s = ''.join(value)
+        i = int(s, 2)
+        print(key, i)
+
+    answer1 = sum(list(map(lambda x: int(''.join(x), 2), cs.mem.values())))
+    print(f'Answer 1: {answer1}')
