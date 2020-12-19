9fabcfddb85549125b6e0a7fc812b2af12e4efa7
Jacob Mastel <jacob.mastel@oregonstate.edu>
2020-12-18 22:14:20 -0800

    First round pass. Messy and doesn't work.

--- day17.py
+++ day17.py
@@ -23,7 +23,7 @@ class ConwayCube:
     coordinates: Dict
 
     def __init__(self, starting_input: Matrix):
-        self.coordinates = defaultdict(lambda: NEWCOOR)
+        self.coordinates = {}
         self.parse_input(starting_input)
 
     def parse_input(self, starting_input: Matrix):
@@ -32,29 +32,69 @@ class ConwayCube:
             for y, point in enumerate(line):
                 self.coordinates[(x, y, z)] = point
 
-    def count_active(self):
+    def count_active(self) -> int:
         count = 0
         for point in self.coordinates.values():
             if point == ACTIVE:
                 count += 1
 
-    def do_round(self):
-        new_dict: defaultdict   = self.coordinates.copy()
-        new_coords: List[Tuple] = []
-        active_neighbors: int   = 0
+        return count
 
-        for coordinate in self.coordinates.keys():
+    def do_round(self):
+        new_dict: Dict          = self.coordinates.copy()
+        new_coords: List[Tuple] = []
+
+        for coordinate, state in self.coordinates.items():
+            active_neighbors: int = 0
+
             for relative_pos in cube_neighbors():
                 new_coord = tuple(sum(x) for x in zip(coordinate, relative_pos))
-                value     = self.coordinates[new_coord]
+                # new_state = self.coordinates[new_coord]
+                if new_coord not in self.coordinates:
+                    new_state = NEWCOOR
+                else:
+                    new_state = self.coordinates[new_coord]
 
-                if value == NEWCOOR:
+                if new_state == NEWCOOR:
                     new_coords.append(new_coord)
-                elif value == ACTIVE:
+                elif new_state == ACTIVE:
                     active_neighbors += 1
+
+            if state == ACTIVE and 2 <= active_neighbors <= 3:
+                new_dict[coordinate] = ACTIVE
+            else:
+                new_dict[coordinate] = INACTIVE
+
+            if state == INACTIVE and active_neighbors == 3:
+                new_dict[coordinate] = ACTIVE
+
+        for coordinate in new_coords:
+            active_neighbors: int = 0
+
+            for relative_pos in cube_neighbors():
+                new_coord = tuple(sum(x) for x in zip(coordinate, relative_pos))
+                if new_coord not in self.coordinates:
+                    new_state = NEWCOOR
+                else:
+                    new_state = self.coordinates[new_coord]
+
+                # We don't care about going further into the unknowns
+                if new_state == NEWCOOR:
+                    continue
+                elif new_state == ACTIVE:
+                    active_neighbors += 1
+
+            if state == INACTIVE and active_neighbors == 3:
+                new_dict[coordinate] = ACTIVE
+                
+        self.coordinates = new_dict.copy()
 
 
 if __name__ == '__main__':
-    puzzle_input = common.read_matrix('day17.txt', str)
+    puzzle_input = common.read_matrix('day17-test.txt', str)
     cube = ConwayCube(puzzle_input)
-    cube.do_round()
+
+    for i in range(0, 2):
+        cube.do_round()
+
+    print(cube.count_active())
