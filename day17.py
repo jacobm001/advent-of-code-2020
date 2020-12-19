b630887424863002483ea84a8f5fd83c27fc1008
Jacob Mastel <jacob.mastel@oregonstate.edu>
2020-12-18 22:32:49 -0800

    Second pass. Still not working

--- day17.py
+++ day17.py
@@ -23,7 +23,7 @@ class ConwayCube:
     coordinates: Dict
 
     def __init__(self, starting_input: Matrix):
-        self.coordinates = {}
+        self.coordinates: defaultdict = defaultdict(lambda: '?')
         self.parse_input(starting_input)
 
     def parse_input(self, starting_input: Matrix):
@@ -40,53 +40,42 @@ class ConwayCube:
 
         return count
 
+    def check_neighbors(self, coordinate) -> int:
+        count: int = 0
+        for relative_pos in cube_neighbors():
+            relative_coordinate = tuple(sum(x) for x in zip(coordinate, relative_pos))
+            relative_state      = self.coordinates[relative_coordinate]
+
+            if relative_state == ACTIVE:
+                count += 1
+
+        return count
+
     def do_round(self):
         new_dict: Dict          = self.coordinates.copy()
         new_coords: List[Tuple] = []
 
-        for coordinate, state in self.coordinates.items():
-            active_neighbors: int = 0
-
-            for relative_pos in cube_neighbors():
-                new_coord = tuple(sum(x) for x in zip(coordinate, relative_pos))
-                # new_state = self.coordinates[new_coord]
-                if new_coord not in self.coordinates:
-                    new_state = NEWCOOR
-                else:
-                    new_state = self.coordinates[new_coord]
-
-                if new_state == NEWCOOR:
-                    new_coords.append(new_coord)
-                elif new_state == ACTIVE:
-                    active_neighbors += 1
+        # overriding the iterator here to try and not have the defaultdict not change the size during run
+        for coordinate, state in tuple(self.coordinates.items()):
+            active_neighbors: int = self.check_neighbors(coordinate)
 
             if state == ACTIVE and 2 <= active_neighbors <= 3:
-                new_dict[coordinate] = ACTIVE
+                # cube remains active
+                continue
+            elif state == INACTIVE and active_neighbors == 2:
+                self.coordinates[coordinate] = ACTIVE
+
+        # handle the new NEWCOOR pieces
+        for coordinate, state in tuple(self.coordinates.items()):
+            if state != NEWCOOR:
+                continue
+
+            active_neighbors: int = self.check_neighbors(coordinate)
+            if active_neighbors == 2:
+                self.coordinates[coordinate] = ACTIVE
             else:
-                new_dict[coordinate] = INACTIVE
+                self.coordinates[coordinate] = INACTIVE
 
-            if state == INACTIVE and active_neighbors == 3:
-                new_dict[coordinate] = ACTIVE
-
-        for coordinate in new_coords:
-            active_neighbors: int = 0
-
-            for relative_pos in cube_neighbors():
-                new_coord = tuple(sum(x) for x in zip(coordinate, relative_pos))
-                if new_coord not in self.coordinates:
-                    new_state = NEWCOOR
-                else:
-                    new_state = self.coordinates[new_coord]
-
-                # We don't care about going further into the unknowns
-                if new_state == NEWCOOR:
-                    continue
-                elif new_state == ACTIVE:
-                    active_neighbors += 1
-
-            if state == INACTIVE and active_neighbors == 3:
-                new_dict[coordinate] = ACTIVE
-                
         self.coordinates = new_dict.copy()
 
 
@@ -94,7 +83,7 @@ if __name__ == '__main__':
     puzzle_input = common.read_matrix('day17-test.txt', str)
     cube = ConwayCube(puzzle_input)
 
-    for i in range(0, 2):
+    for i in range(0, 7):
         cube.do_round()
 
     print(cube.count_active())
